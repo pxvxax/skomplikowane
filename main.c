87edf5fa63b4d42b128fe6bd2e596e482b77fda5
Pawel Szczepanski <pxvxax@gmail.com>
2018-10-12 11:00:38 +0200

    co

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+    char znak;
+    int wartosc_calkowita;
+    double wartosc_wymierna;
+
+    scanf("%c%i%lf",
+          &znak,
+          &wartosc_calkowita,
+          &wartosc_wymierna);
+
+    printf(
+            "znak = %c,"
+            "\n liczba całkowita = %i, liczba wymierna = %lf",
+    znak,
+            wartosc_calkowita,
+            wartosc_wymierna
+    );
+
+    return 0;
+
+
+}
